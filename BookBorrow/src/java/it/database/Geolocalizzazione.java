d5fb486bb6cfc26e341e380e58ad11306a25a642
insan3 <insan3@insan3-Aspire-E5-571G>
2015-09-06 23:46:09 +0200

    modifica classe geolocalizzazione: creato metodo per calcolare distanza in Km tra due coordinate

--- BookBorrow/src/java/it/database/Geolocalizzazione.java
+++ BookBorrow/src/java/it/database/Geolocalizzazione.java
@@ -23,7 +23,7 @@ public class Geolocalizzazione {
         do {
             try {
                 count++;
-                
+
                 coord = GeoCodeResolver.findCoordForAddress(indirizzo);
 
                 lat = coord.getLat();
@@ -33,6 +33,37 @@ public class Geolocalizzazione {
                 count++;
             }
         } while (!(coord instanceof LatLng) || count <= 10);
-        return lat+" "+lon;
+
+        return lat + " " + lon;
     }
+
+    public static double getDistance(String coord1, String coord2) {
+        //spiegazione matematica--> http://www.robertobigoni.it/Matematica/Sferica/sferica.html
+        
+        LatLng c1 = new LatLng(new BigDecimal(coord1.split(" ")[0]), new BigDecimal(coord1.split(" ")[1]));
+        LatLng c2 = new LatLng(new BigDecimal(coord2.split(" ")[0]), new BigDecimal(coord2.split(" ")[1]));
+
+        double out;
+        
+        double R = 6371; //raggio terrestre medio
+
+        double lat_c1, lat_c2, lon_c1, lon_c2;
+        
+        double fi;
+      
+       
+        //converto coordinate in radianti double così da poter applicare la formula
+        lat_c1 = Math.PI * c1.getLat().doubleValue() / 180;
+        lat_c2 = Math.PI * c2.getLat().doubleValue() / 180;
+        lon_c1 = Math.PI * c1.getLng().doubleValue() / 180;
+        lon_c2 = Math.PI * c2.getLng().doubleValue() / 180;
+
+       
+        fi = Math.abs(lon_c1 - lon_c2); //calcolo angolo tra due punti
+      
+        out =R* (Math.acos(Math.sin(lat_c2) * Math.sin(lat_c1)+ Math.cos(lat_c2) * Math.cos(lat_c1) * Math.cos(fi)));
+
+        return out;
+    }
+
 }
