70e820933c9d02db14e6ca9f9a97cf45c7df16a9
insan3 <insan3@insan3-Aspire-E5-571G>
2015-09-09 14:49:37 +0200

    delete user ok

--- BookBorrow/src/java/it/functions/OperazioniAdmin.java
+++ BookBorrow/src/java/it/functions/OperazioniAdmin.java
@@ -5,14 +5,18 @@
  */
 package it.functions;
 
+import it.database.Connessione;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Arrays;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.swing.JOptionPane;
 
 /**
  *
@@ -48,29 +52,23 @@ public class OperazioniAdmin extends HttpServlet {
             out.println("</html>");
         }
     }
+    /*
 
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
+     @Override
+     protected void doGet(HttpServletRequest request, HttpServletResponse response)
+     throws ServletException, IOException {
+     processRequest(request, response);
         
-        switch ((String) request.getSession().getAttribute("Operazione")) {
-            case "elimina": eliminazione(request,response);
-                break;
-            case "gestisci": gestione(request,response);
-                break;
-            case "statistiche": statistiche(request,response);
-                break;
-        }
-    }
+     switch ((String) request.getSession().getAttribute("Operazione")) {
+     case "elimina": eliminazione(request,response);
+     break;
+     case "gestisci": gestione(request,response);
+     break;
+     case "statistiche": statistiche(request,response);
+     break;
+     }
+     }
+     */
 
     /**
      * Handles the HTTP <code>POST</code> method.
@@ -83,15 +81,27 @@ public class OperazioniAdmin extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
-        JOptionPane.showMessageDialog(null, "doPost"+request.getParameter("delete").split(":")[1]);
-        processRequest(request, response);
+
+        String mail = request.getParameter("delete");
+
+        //processRequest(request, response);
         switch ((String) request.getSession().getAttribute("Operazione")) {
-            case "elimina": eliminazione(request,response);
+            case "elimina":
+        {
+            try {
+                eliminazione(mail);
+                response.sendRedirect("deleteUser.jsp");
+            } catch (ClassNotFoundException | SQLException ex) {
+                response.sendRedirect("errorPage.jsp");
+            }
+        }
+
                 break;
-            case "gestisci": gestione(request,response);
+            case "gestisci":
+                gestione(request, response);
                 break;
-            case "statistiche": statistiche(request,response);
+            case "statistiche":
+                statistiche(request, response);
                 break;
         }
     }
@@ -110,10 +120,12 @@ public class OperazioniAdmin extends HttpServlet {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    private void eliminazione(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String s = (String) request.getSession().getAttribute("elimSel");
-        
-        
+    private void eliminazione(String mail) throws ClassNotFoundException, SQLException{
+        String sql = "DELETE FROM book_user WHERE email='" + mail + "'";
+        try (Connection con = Connessione.getConnection()) {
+            Statement stmt =con.createStatement();
+            stmt.executeUpdate(sql);
+        }
     }
 
     private void statistiche(HttpServletRequest request, HttpServletResponse response) {
